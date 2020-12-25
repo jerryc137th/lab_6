c458adbe9850e8a6aa42bc1a4ac85175dca9be81
jerryc137th <76035472+jerryc137th@users.noreply.github.com>
2020-12-25 17:34:28 +0700

    no.14

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	string a = "BCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int n=0;
+	int i = 0;
+	if(x=='A'){
+		return 'Z';
+	}
+	while(i<=24){
+		if( x == a[i]){
+			n = 1;
+		}
+		i++;
+	}
+	if(n==1){
+		return x-1;
+	}
+	if(n!=1){
+		return '0';
+	}
+	return '0';
 }
 
 int main(){
